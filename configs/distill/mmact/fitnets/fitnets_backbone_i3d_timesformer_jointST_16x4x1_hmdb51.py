4decbf00e0fa81127f44ce2c2f54dbd96bb3bf37
Zeng Runhao <zengrunhao@gpu013.scut-smil.cn>
2024-03-27 00:33:12 +0800

    fix configs

--- configs/distill/mmact/fitnets/fitnets_backbone_i3d_timesformer_jointST_16x4x1_hmdb51.py
+++ configs/distill/mmact/fitnets/fitnets_backbone_i3d_timesformer_jointST_16x4x1_hmdb51.py
@@ -36,14 +36,14 @@ model = dict(
             loss_s3=dict(type='MSELoss', loss_weight=1),
             loss_s4=dict(type='MSELoss', loss_weight=1)),
         connectors=dict(
-            loss_s1_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=3137, t_dim=16, st_size=(4,56,56), embed_dim=256),
-            loss_s2_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=3137, t_dim=16, st_size=(2,28,28), embed_dim=512),
-            loss_s3_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=3137, t_dim=16, st_size=(2,14,14), embed_dim=1024),
-            loss_s4_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=3137, t_dim=16, st_size=(2,7,7), embed_dim=2048),
-            loss_s1_tfeat=dict(type='CNNFeatTransfer', in_channel=256, st_size=(4,56,56), seq_len=3137, t_dim=16, embed_dim=768, is_student=False),
-            loss_s2_tfeat=dict(type='CNNFeatTransfer', in_channel=512, st_size=(2,28,28), seq_len=3137, t_dim=16, embed_dim=768, is_student=False),
-            loss_s3_tfeat=dict(type='CNNFeatTransfer', in_channel=1024, st_size=(2,14,14), seq_len=3137, t_dim=16, embed_dim=768, is_student=False),
-            loss_s4_tfeat=dict(type='CNNFeatTransfer', in_channel=2048, st_size=(2,7,7), seq_len=3137, t_dim=16, embed_dim=768, is_student=False)),
+            loss_s1_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=1569, t_dim=8, st_size=(8,56,56), embed_dim=256),
+            loss_s2_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=1569, t_dim=8, st_size=(8,28,28), embed_dim=512),
+            loss_s3_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=1569, t_dim=8, st_size=(8,14,14), embed_dim=1024),
+            loss_s4_sfeat=dict(type='TransFeatTransfer', in_channel=768, seq_len=1569, t_dim=8, st_size=(8,7,7), embed_dim=2048),
+            loss_s1_tfeat=dict(type='CNNFeatTransfer', in_channel=256, st_size=(8,56,56), seq_len=1569, t_dim=8, embed_dim=768, is_student=False),
+            loss_s2_tfeat=dict(type='CNNFeatTransfer', in_channel=512, st_size=(8,28,28), seq_len=1569, t_dim=8, embed_dim=768, is_student=False),
+            loss_s3_tfeat=dict(type='CNNFeatTransfer', in_channel=1024, st_size=(8,14,14), seq_len=1569, t_dim=8, embed_dim=768, is_student=False),
+            loss_s4_tfeat=dict(type='CNNFeatTransfer', in_channel=2048, st_size=(8,7,7), seq_len=1569, t_dim=8, embed_dim=768, is_student=False)),
         loss_forward_mappings=dict(
             loss_s1=dict(
                 s_feature=dict(from_student=True, recorder='bb_s1', connector='loss_s1_sfeat'),
